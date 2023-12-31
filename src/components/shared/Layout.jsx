6d6b5b66ec05132e7292cce996911e0fdaea7490
vineetb0901 <140053679+vineetb0901@users.noreply.github.com>
2023-12-31 14:32:43 +0530

    completed header

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 export default function Layout() {
     return (
         <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
-            <Sidebar/>
-            <div className="p-4">
-            <div className="bg-sky-100">header</div>
+            <Sidebar />
+            <div className="flex-1">
+                <Header />
+                <div className="p-4">
+                    <Outlet />
+                </div>
             </div>
 
-            <div>{<Outlet/>}</div>
+
         </div>
+        
     )
 }
+
+
